ce92f54023337f2a00537749653d986f1f73db2d
Randy Oest <oest@amazingrando.com>
2017-07-21 08:41:45 -0400

    ES6: Modified WPT to not run w/o a key; ES6 syntax

--- gulp-tasks/gulp-tests.js
+++ gulp-tasks/gulp-tests.js
@@ -5,12 +5,12 @@
   'use strict';
 
   // Performance Testing
-  var ngrok = require('ngrok');
+  const ngrok = require('ngrok');
   const psi = require('psi');
-  var wpt = require('webpagetest');
-  var spawn = require("gulp-spawn");
+  const wpt = require('webpagetest');
+  const spawn = require("gulp-spawn");
 
-  module.exports = function (gulp, config) {
+  module.exports = (gulp, config) => {
 
     // -----------------------------------------------------------------------------
     // Performance test: PageSpeed Insights
@@ -24,7 +24,7 @@
     //
     // http://tldp.org/LDP/abs/html/exit-status.html
     // -----------------------------------------------------------------------------
-    gulp.task('psi', 'Performance: PageSpeed Insights', function() {
+    gulp.task('psi', 'Performance: PageSpeed Insights', () => {
       // Set up a public tunnel so PageSpeed can see the local site.
       return ngrok.connect(4000, function (err_ngrok, url) {
         console.log('ngrok - serving your site from ' + url);
@@ -33,7 +33,7 @@
         psi.output(url, {
           strategy: 'mobile',
           threshold: 90
-        }, function (err_psi, data) {
+        }, (err_psi, data) => {
           // Log any potential errors and return a FAILURE.
           if (err_psi) {
             console.log(err_psi);
@@ -52,33 +52,35 @@
     // Initializes a public tunnel so the PageSpeed service can access your local
     // site, then it tests the site. This task outputs the standard PageSpeed results.
     // -----------------------------------------------------------------------------
-    gulp.task('wpt', 'Performance: WebPageTest.org', function () {
-      if (!config.wpt.key) {
+    gulp.task('wpt', 'Performance: WebPageTest.org', () => {
+
+      if (config.wpt.key && config.wpt.key !== null) {
+        const wpt_test = wpt('www.webpagetest.org', config.wpt.key);
+
+        // Set up a public tunnel so WebPageTest can see the local site.
+        return ngrok.connect(4000, (err_ngrok, url) => {
+          console.log('ngrok - serving your site from ' + url);
+
+          // The `url` variable was supplied by ngrok.
+          wpt_test.runTest(url, (err_wpt, data_wpt) => {
+            // Log any potential errors and return a FAILURE.
+            if (err_wpt) {
+              console.log(err_wpt);
+              process.exit(1);
+            }
+
+            // Open window to results.
+            const wpt_results = 'http://www.webpagetest.org/result/' + data_wpt.data.testId;
+            console.log('✔︎  Opening results page: ' + wpt_results);
+
+            // Note to developer.
+            console.log('⚠️  Please keep this process running until WPT is finished.');
+            console.log('⚠️  Once the results load, hit Control + C to kill this process.');
+          });
+        });
+      } else {
         console.log('Missing wptkey env variable.');
       }
-      var wpt_test = wpt('www.webpagetest.org', config.wpt.key);
-
-      // Set up a public tunnel so WebPageTest can see the local site.
-      return ngrok.connect(4000, function (err_ngrok, url) {
-        console.log('ngrok - serving your site from ' + url);
-
-        // The `url` variable was supplied by ngrok.
-        wpt_test.runTest(url, function(err_wpt, data_wpt) {
-          // Log any potential errors and return a FAILURE.
-          if (err_wpt) {
-            console.log(err_wpt);
-            process.exit(1);
-          }
-
-          // Open window to results.
-          var wpt_results = 'http://www.webpagetest.org/result/' + data_wpt.data.testId;
-          console.log('✔︎  Opening results page: ' + wpt_results);
-
-          // Note to developer.
-          console.log('⚠️  Please keep this process running until WPT is finished.');
-          console.log('⚠️  Once the results load, hit Control + C to kill this process.');
-        });
-      });
     });
 
     gulp.task('qa', 'Run all quality checks and tests.', ['psi', 'wpt']);
